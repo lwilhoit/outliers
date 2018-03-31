d7763f1861152c1058d4bd6ffa92bfdf8e8f6976
Larry Wilhoit <Larry.Wilhoit@cdpr.ca.gov>
2018-03-31 07:11:50 -0700

    Moved repository from outliers/gui

--- old/exc.py
+++
@@ -1,7 +0,0 @@
-try:
-    x = int(input('Enter the first number: '))
-    y = int(input('Enter the second number: '))
-    print(x / y)
-except:
-    print("The second number can't be zero!")
-    raise
